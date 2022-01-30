433dbdfb18bc7c7114bb204d6c8eb7354ab0d9ff
Gregor Wendland <gregor@gewend.de>
2022-01-30 07:02:13 +0100

    bugfix

--- metadata.php
+++ metadata.php
@@ -10,7 +10,7 @@
 /**
  * Metadata version
  */
-$sMetadataVersion = '2'; // see https://docs.oxid-esales.com/developer/en/6.0/modules/skeleton/metadataphp/version20.html
+$sMetadataVersion = '2.0'; // see https://docs.oxid-esales.com/developer/en/6.0/modules/skeleton/metadataphp/version20.html
 
 /**
  * Module information
@@ -46,8 +46,6 @@ $aModule = array(
 		array('group' => 'gw_oxid_actions_extended', 'name' => 'gw_oxid_actions_extended_randombanners', 'type' => 'str', 'value' => '0'),
     ),
 	'events'		=> array(
-    ),
-    'files'			=> array(
     ),
 	'blocks' => array(
 		// frontend
